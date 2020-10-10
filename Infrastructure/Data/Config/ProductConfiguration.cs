32f3f7df47b3f7ace07e6eef904a81c64491143d
Uriel Rodriguez Usma <urodriguez@heinsohn.com.co>
2020-10-10 17:19:25 -0500

    Se anexa data seed

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(180);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.PictureUrl).IsRequired();
+            builder.HasOne(p => p.ProductBrand).WithMany().HasForeignKey(f => f.ProductBrandId);
+            builder.HasOne(p => p.ProductType).WithMany().HasForeignKey(f => f.ProductTypeId);
+        }
+    }
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,8 +1,10 @@
 ﻿using Core.Entities;
+using Infrastructure.Data.Config;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -10,7 +12,15 @@ namespace Infrastructure.Data
     public class StoreContext : DbContext
     {
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
-
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductsBrand { get; set; }
+        public DbSet<ProductType> ProductsType { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
